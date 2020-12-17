059f981e0ebb441d2d0c4415393f4c3197ad6f2d
Alex Sheverdin <alex@skalelabs.com>
2020-12-17 22:37:36 +0200

    SKALE-3629 Fix typo

--- configs/__init__.py
+++ configs/__init__.py
@@ -3,5 +3,5 @@ DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 API_HOST = 'localhost'
 API_PORT = '3007'
-API_CONT_HEALTH_URL = '/healthchecks/containers'
+API_CONT_HEALTH_URL = 'healthchecks/containers'
 API_TIMEOUT = 2  # in seconds
