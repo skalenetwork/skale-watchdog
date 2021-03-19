a148a041770b5f0176e5e254dca6ca03ab942171
badrogger <ivan@skalelabs.com>
2021-03-19 18:14:09 +0200

    SKALE-3997 Unite endpoints routes into dict

--- configs/__init__.py
+++ configs/__init__.py
@@ -3,6 +3,17 @@ DATETIME_FORMAT = "%Y-%m-%d %H:%M:%S"
 
 API_HOST = 'localhost'
 API_PORT = '3007'
+
+HEALTHCHECKS_ROUTES = {
+    'containers': 'healthchecks/containers',
+    'sgx': 'api/sgx/info',
+    'schains': 'api/schains/healthchecks',
+    'hardware': 'hardware',
+    'endpoint': 'endpoint-info',
+    'schain_containers_versions': 'schain-containers-versions',
+    'meta_info': 'meta-info'
+}
+
 API_CONT_HEALTH_URL = 'healthchecks/containers'
 API_SGX_HEALTH_URL = 'api/sgx/info'
 API_SCHAINS_HEALTH_URL = 'api/schains/healthchecks'
@@ -10,4 +21,5 @@ API_HARDWARE_INFO_URL = 'hardware'
 API_ENDPOINT_INFO_URL = 'endpoint-info'
 API_SCHAIN_CONTAINERS_VERSIONS_URL = 'schain-containers-versions'
 API_META_INFO_URL = 'meta-info'
+
 API_TIMEOUT = 1000  # in seconds
--- main.py
+++ main.py
@@ -19,16 +19,13 @@
 
 import logging
 
+import time
+
 from flask import Flask, request
 
 from configs.flask import FLASK_APP_HOST, FLASK_APP_PORT, FLASK_DEBUG_MODE
 from utils.helper import init_default_logger, get_healthcheck_from_skale_api
-from configs import (
-    API_CONT_HEALTH_URL, API_SGX_HEALTH_URL, API_SCHAINS_HEALTH_URL,
-    API_HARDWARE_INFO_URL, API_ENDPOINT_INFO_URL, API_META_INFO_URL,
-    API_SCHAIN_CONTAINERS_VERSIONS_URL
-)
-
+from conifgs import HEALTHCHECKS_ROUTES
 init_default_logger()
 
 logger = logging.getLogger(__name__)
@@ -41,43 +38,45 @@ app.use_reloader = False
 @app.route('/status/core', methods=['GET'])
 def containers_core_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_CONT_HEALTH_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['containers'])
 
 
 @app.route('/status/sgx', methods=['GET'])
 def sgx_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_SGX_HEALTH_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['sgx'])
 
 
 @app.route('/status/schains', methods=['GET'])
 def schains_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_SCHAINS_HEALTH_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['schains'])
 
 
 @app.route('/status/hardware', methods=['GET'])
 def hardware_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_HARDWARE_INFO_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['hardware'])
 
 
 @app.route('/status/endpoint', methods=['GET'])
 def endpoint_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_ENDPOINT_INFO_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['endpoint'])
 
 
 @app.route('/status/schain-containers-versions', methods=['GET'])
 def schain_containers_versions_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_SCHAIN_CONTAINERS_VERSIONS_URL)
+    return get_healthcheck_from_skale_api(
+        HEALTHCHECKS_ROUTES['schain-containers-versions']
+    )
 
 
 @app.route('/status/meta-info', methods=['GET'])
 def meta_status():
     logger.debug(request)
-    return get_healthcheck_from_skale_api(API_META_INFO_URL)
+    return get_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['meta-info'])
 
 
 if __name__ == '__main__':
