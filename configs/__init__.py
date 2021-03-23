2e5d8f3136e3872693ef2c02490aee34014087bb
badrogger <ivan@skalelabs.com>
2021-03-23 18:29:26 +0200

    SKALE-3997 Remove redundant constants

--- configs/__init__.py
+++ configs/__init__.py
@@ -14,12 +14,4 @@ HEALTHCHECKS_ROUTES = {
     'meta': 'meta-info'
 }
 
-API_CONT_HEALTH_URL = 'healthchecks/containers'
-API_SGX_HEALTH_URL = 'api/sgx/info'
-API_SCHAINS_HEALTH_URL = 'api/schains/healthchecks'
-API_HARDWARE_INFO_URL = 'hardware'
-API_ENDPOINT_INFO_URL = 'endpoint-info'
-API_SCHAIN_CONTAINERS_VERSIONS_URL = 'schain-containers-versions'
-API_META_INFO_URL = 'meta-info'
-
 API_TIMEOUT = 1000  # in seconds
