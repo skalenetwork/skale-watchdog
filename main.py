98bd21dbae7b55307d40ee4900c0787bb4d7ef03
badrogger <ivan@skalelabs.com>
2021-03-29 10:43:41 +0300

    SKALE-3997 Improve 500 error handling

--- main.py
+++ main.py
@@ -18,16 +18,19 @@
 #   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
+import time
 
 
-from flask import Flask, request
+from flask import Flask, g, request
+from werkzeug.exceptions import InternalServerError
 
 import utils.background_tasks  # noqa
+from configs import HEALTHCHECKS_ROUTES
 from configs.flask import FLASK_APP_HOST, FLASK_APP_PORT, FLASK_DEBUG_MODE
 from utils.healthchecks import get_healthcheck_from_skale_api
 from utils.log import init_default_logger
+from utils.structures import construct_err_response
 
-from configs import HEALTHCHECKS_ROUTES
 init_default_logger()
 
 logger = logging.getLogger(__name__)
@@ -35,6 +38,24 @@ app = Flask(__name__)
 app.port = FLASK_APP_PORT
 app.host = FLASK_APP_HOST
 app.use_reloader = False
+
+
+@app.before_request
+def before_request():
+    g.request_start_time = time.time()
+
+
+@app.teardown_request
+def teardown_request(response):
+    elapsed = int(time.time() - g.request_start_time)
+    logger.info(f'Request time elapsed: {elapsed}s')
+    return response
+
+
+@app.errorhandler(InternalServerError)
+def handle_500(e):
+    original = getattr(e, "original_exception", None)
+    return construct_err_response(status=500, err=original).to_flask_response()
 
 
 @app.route('/status/core', methods=['GET'])
