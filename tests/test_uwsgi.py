c2e17a43953301e1669f66afb5195c7a19d579d2
badrogger <ivan@skalelabs.com>
2021-05-12 08:09:02 +0300

    Fix tests

--- tests/test_uwsgi.py
+++ tests/test_uwsgi.py
@@ -94,7 +94,7 @@ def test_unsuccessfull_request(skale_api_success):
     ts_diff = timer() - start_ts
 
     data = response.json()
-    assert data == {'data': None, 'error': 'Request to http://localhost:3007/meta-info failed, status code: 400'}  # noqa
+    assert data == {'data': None, 'error': 'Request to http://localhost:3007/meta-info failed, code: 400'}  # noqa
     assert ts_diff < 2
 
 
