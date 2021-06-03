55777f3611347d9e8fbd03b0b30b25d6490df721
Alex Sheverdin <alex@skalelabs.com>
2021-06-03 20:09:03 +0300

    SKALE-4196 Check if IMA container is running

--- utils/ima.py
+++ utils/ima.py
@@ -5,6 +5,8 @@ import os
 from configs import SCHAINS_DIR_PATH, SCHAINS_PREFIX
 import logging
 from utils.structures import construct_ok_response
+from utils.healthchecks import request_healthcheck_from_skale_api
+from configs import HEALTHCHECKS_ROUTES
 
 logger = logging.getLogger(__name__)
 
@@ -55,26 +57,45 @@ def get_schain_config_file_name(schain_name):
 
 
 def get_ima_healthchecks():
+    ima_containers = get_ima_containers()
     ima_healthchecks = []
     for schain_name in os.listdir(SCHAINS_DIR_PATH):
         print(schain_name)
         error_text = None
         ima_healthcheck = []
+        container_name = f'skale_ima_{schain_name}'
 
-        try:
-            ima_port = get_ima_monitoring_port(schain_name)
-        except KeyError as err:
-            logger.exception(err)
-            error_text = repr(err)
+        cont_data = next((item for item in ima_containers if item["name"] == container_name), None)
+        if cont_data is None:
+            continue
+        elif cont_data['state'] != 'running':
+            error_text = 'Docker container is not running'
         else:
-            if ima_port is None:
-                continue
-            endpoint = f'ws://localhost:{ima_port}'
             try:
-                ima_healthcheck = request_ima_healthcheck(endpoint)
-            except Exception as err:
+                ima_port = get_ima_monitoring_port(schain_name)
+            except KeyError as err:
                 logger.exception(err)
                 error_text = repr(err)
+            else:
+                if ima_port is None:
+                    continue
+                endpoint = f'ws://localhost:{ima_port}'
+                try:
+                    ima_healthcheck = request_ima_healthcheck(endpoint)
+                except Exception as err:
+                    logger.exception(err)
+                    error_text = repr(err)
         ima_healthchecks.append({schain_name: {'error': error_text,
                                                'last_ima_errors': ima_healthcheck}})
     return construct_ok_response(ima_healthchecks)
+
+
+def get_ima_containers():
+    response = request_healthcheck_from_skale_api(HEALTHCHECKS_ROUTES['containers'])
+    containers = response.data['data']
+    ima_containers = [{container['name']: container['state']['Status']} for container in containers]
+    print(ima_containers)
+    return ima_containers
+
+
+
