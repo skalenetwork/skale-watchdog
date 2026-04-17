e3001f393f497fcb2581cf3dd79bb4a2c6c519ba
Alex Sheverdin <alex@skalelabs.com>
2026-04-17 16:20:22 +0100

    Refactor tests for compose_hiding_patterns to use is_passive and is_fair mocks

--- tests/test_log.py
+++ tests/test_log.py
@@ -4,10 +4,9 @@ from utils.log import compose_hiding_patterns
 
 
 def test_compose_hiding_patterns_active_skale():
-    with patch('utils.log.INTERNAL_ST') as mock_internal_st, \
+    with patch('utils.log.is_passive', return_value=False), \
+         patch('utils.log.is_fair', return_value=False), \
          patch('utils.log.get_settings') as mock_get_settings:
-        mock_internal_st.node_mode = 'active'
-        mock_internal_st.node_type = 'skale'
         mock_settings = MagicMock()
         mock_settings.sgx_url = 'http://192.168.1.100:1234'
         mock_settings.endpoint = 'http://10.0.0.1:8545'
@@ -18,37 +17,26 @@ def test_compose_hiding_patterns_active_skale():
 
 
 def test_compose_hiding_patterns_passive_skale():
-    with patch('utils.log.INTERNAL_ST') as mock_internal_st, \
+    with patch('utils.log.is_passive', return_value=True), \
+         patch('utils.log.is_fair', return_value=False), \
          patch('utils.log.get_settings') as mock_get_settings:
-        mock_internal_st.node_mode = 'passive'
-        mock_internal_st.node_type = 'skale'
         mock_settings = MagicMock()
         mock_settings.endpoint = 'http://10.0.0.2:8545'
         mock_get_settings.return_value = mock_settings
         patterns = compose_hiding_patterns()
-        assert '[SGX_IP]' not in patterns.values()
+        assert 'sgx_url' not in str(mock_get_settings.call_args_list[0])
         assert patterns.get('10.0.0.2') == '[ETH_IP]'
+        mock_get_settings.assert_called_once()
 
 
 def test_compose_hiding_patterns_active_fair():
-    with patch('utils.log.INTERNAL_ST') as mock_internal_st, \
+    with patch('utils.log.is_passive', return_value=False), \
+         patch('utils.log.is_fair', return_value=True), \
          patch('utils.log.get_settings') as mock_get_settings:
-        mock_internal_st.node_mode = 'active'
-        mock_internal_st.node_type = 'fair'
         mock_settings = MagicMock()
         mock_settings.sgx_url = 'http://192.168.1.105:1234'
         mock_get_settings.return_value = mock_settings
         patterns = compose_hiding_patterns()
         assert patterns.get('192.168.1.105') == '[SGX_IP]'
-        assert '[ETH_IP]' not in patterns.values()
+        mock_get_settings.assert_called_once()
 
-
-def test_compose_hiding_patterns_passive_fair():
-    with patch('utils.log.INTERNAL_ST') as mock_internal_st, \
-         patch('utils.log.get_settings') as mock_get_settings:
-        mock_internal_st.node_mode = 'passive'
-        mock_internal_st.node_type = 'fair'
-        mock_get_settings.return_value = MagicMock()
-        patterns = compose_hiding_patterns()
-        assert '[SGX_IP]' not in patterns.values()
-        assert '[ETH_IP]' not in patterns.values()
